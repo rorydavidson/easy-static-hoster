189119876fc669238fc49be50611a881ddbd757e
Rory Davidson <rory.davidson@gmail.com>
2026-03-06 14:40:32 +0100

    Another fix

--- generator/entrypoint.sh
+++ generator/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # The /content volume may be created by Docker as root on a fresh host.
 # Fix ownership so appuser can write index.html, then drop privileges.
-chown appuser:appgroup /content
+chown -R appuser:appgroup /content
 
 exec su-exec appuser python generate.py
